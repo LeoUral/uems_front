668637a97f1608bb9fbeaa6a9f4fef9ed1ec0744
Leonid <49805034+LeoUral@users.noreply.github.com>
2021-04-16 15:16:51 +0500

    create new project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/style/main.css';
+
+function App() {
+    return (
+        <div className="App" >
+            Hello word!!!
+        </div>
+    );
+}
+
+export default App;
