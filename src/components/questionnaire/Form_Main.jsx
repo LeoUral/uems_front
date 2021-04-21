1b116b58c80110d90044d2e7964adae98721e8f7
Leonid <49805034+LeoUral@users.noreply.github.com>
2021-04-21 16:44:32 +0500

    в процессе формы анкеты

--- src/components/questionnaire/Form_Main.jsx
+++ src/components/questionnaire/Form_Main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert, Container, Form, Row } from 'react-bootstrap';
+import { Alert, Container, Form, Row, Button } from 'react-bootstrap';
 import CheckForm from './CheckForm';
 import InputForm from './InputForm';
 import SelectForm from './SelectForm';
@@ -30,76 +30,246 @@ export default class Form_Main extends React.Component {
                 <div className="modal_window">
                     {/* <div className="shadow"></div> */}
                     <Form className="form_main">
-                        <Alert variant="success">
-                            <Alert.Heading > Form Main </Alert.Heading>
+                        <Alert variant="dark">
+                            <Alert.Heading > Общаяя информация </Alert.Heading>
                         </Alert>
-                        <Row>
-                            <InputForm
-                                id={1}
-                                width={6}
-                                show={true}
-                                // verify="number"
-                                label="Полное наименование предприятия"
-                                placeholder="Полное наименование предприятия"
-                                description="Полное наименование предприятия"
-                                value={value}
-                                onChangeValue={this.doChangeValue}
-                            />
-                        </Row>
+                        <Container className="container_form_min">
+                            <Form.Group>
+                                <Row>
+                                    <InputForm
+                                        id={1}
+                                        width={12}
+                                        show={true}
+                                        // verify="number"
+                                        label="Полное наименование предприятия"
+                                        placeholder="Полное наименование предприятия"
+                                        description="Предприятие"
+                                        value={value}
+                                        onChangeValue={this.doChangeValue}
+                                    />
+                                </Row>
+                                <Row>
+                                    <InputForm
+                                        id={2}
+                                        width={9}
+                                        show={true}
+                                        // verify="number"
+                                        label=""
+                                        placeholder="ФИО руководителя"
+                                        description="Предприятие"
+                                        value={value}
+                                        onChangeValue={this.doChangeValue}
+                                    />
+                                    <SelectForm
+                                        id={3}
+                                        width={3}
+                                        show={true}
+                                        label=""
+                                        placeholder="Должность"
+                                        description="Предприятие"
+                                        option="Директор, Генеральный директор, Президент" //*список для выбора
+                                        value=""
+                                        onChangeValue={this.doChangeValue}
+                                    />
+                                </Row>
+                                <Row>
+                                    <InputForm
+                                        id={4}
+                                        width={4}
+                                        show={true}
+                                        // verify="number"
+                                        label=""
+                                        placeholder="Телефон"
+                                        description="Предприятие"
+                                        value={value}
+                                        onChangeValue={this.doChangeValue}
+                                    />
+                                    <InputForm
+                                        id={4}
+                                        width={8}
+                                        show={true}
+                                        // verify="number"
+                                        label=""
+                                        placeholder="E-Mail"
+                                        description="Предприятие"
+                                        value={value}
+                                        onChangeValue={this.doChangeValue}
+                                    />
+                                </Row>
+                            </Form.Group>
+                            <Form.Group>
+                                <Row>
+                                    <InputForm
+                                        id={4}
+                                        width={4}
+                                        show={true}
+                                        verify="number"
+                                        label=""
+                                        placeholder="ИНН"
+                                        description="Предприятие"
+                                        value={value}
+                                        onChangeValue={this.doChangeValue}
+                                    />
+                                    <InputForm
+                                        id={4}
+                                        width={4}
+                                        show={true}
+                                        verify="number"
+                                        label=""
+                                        placeholder="КПП"
+                                        description="Предприятие"
+                                        value={value}
+                                        onChangeValue={this.doChangeValue}
+                                    />
+                                    <InputForm
+                                        id={4}
+                                        width={4}
+                                        show={true}
+                                        verify="number"
+                                        label=""
+                                        placeholder="ОГРН"
+                                        description="Предприятие"
+                                        value={value}
+                                        onChangeValue={this.doChangeValue}
+                                    />
+                                </Row>
+                            </Form.Group>
+                            <Form.Group>
+                                <Row>
+                                    <InputForm
+                                        id={4}
+                                        width={12}
+                                        show={true}
+                                        // verify="number"
+                                        label="Контактное лицо"
+                                        placeholder="ФИО"
+                                        description="Контактное лицо"
+                                        value={value}
+                                        onChangeValue={this.doChangeValue}
+                                    />
+                                </Row>
+                                <Row>
+                                    <InputForm
+                                        id={4}
+                                        width={4}
+                                        show={true}
+                                        // verify="number"
+                                        label=""
+                                        placeholder="Должность"
+                                        description="Контактное лицо"
+                                        value={value}
+                                        onChangeValue={this.doChangeValue}
+                                    />
+                                    <InputForm
+                                        id={4}
+                                        width={4}
+                                        show={true}
+                                        // verify="number"
+                                        label=""
+                                        placeholder="Телефон"
+                                        description="Контактное лицо"
+                                        value={value}
+                                        onChangeValue={this.doChangeValue}
+                                    />
+                                    <InputForm
+                                        id={4}
+                                        width={4}
+                                        show={true}
+                                        // verify="number"
+                                        label=""
+                                        placeholder="E mail"
+                                        description="Контактное лицо"
+                                        value={value}
+                                        onChangeValue={this.doChangeValue}
+                                    />
+                                </Row>
+                            </Form.Group>
+                            <div> 1. Адрес расположения производственных площадей и технических служб предприятия-изготовителя </div>
+                            <Form.Group>
+                                <Row>
+                                    <InputForm
+                                        id={4}
+                                        width={4}
+                                        show={true}
+                                        verify="number"
+                                        label=""
+                                        placeholder="Индекс"
+                                        description="Предприятие"
+                                        value={value}
+                                        onChangeValue={this.doChangeValue}
+                                    />
+                                    <SelectForm
+                                        id={3}
+                                        width={4}
+                                        show={true}
+                                        label=""
+                                        placeholder="Срана"
+                                        description="Предприятие"
+                                        option="Россия, не Россия"
+                                        value=""
+                                        onChangeValue={this.doChangeValue}
+                                    />
+                                    <SelectForm
+                                        id={3}
+                                        width={4}
+                                        show={true}
+                                        label=""
+                                        placeholder="Город"
+                                        description="Предприятие"
+                                        option="Города России"
+                                        value=""
+                                        onChangeValue={this.doChangeValue}
+                                    />
+                                </Row>
+                                <Row>
+                                    <InputForm
+                                        id={4}
+                                        width={6}
+                                        show={true}
+                                        // verify="number"
+                                        label=""
+                                        placeholder="Улица"
+                                        description="Предприятие"
+                                        value={value}
+                                        onChangeValue={this.doChangeValue}
+                                    />
+                                    <InputForm
+                                        id={4}
+                                        width={3}
+                                        show={true}
+                                        verify="number"
+                                        label=""
+                                        placeholder="Дом"
+                                        description="Предприятие"
+                                        value={value}
+                                        onChangeValue={this.doChangeValue}
+                                    />
+                                    <InputForm
+                                        id={4}
+                                        width={3}
+                                        show={true}
+                                        // verify="number"
+                                        label=""
+                                        placeholder="Корпус"
+                                        description="Предприятие"
+                                        value={value}
+                                        onChangeValue={this.doChangeValue}
+                                    />
+                                </Row>
+                            </Form.Group>
+                            <Form.Group>
+                                <Row>
+                                    <Container>
+                                        <Button variant="outline-dark">Сохранить информацию</Button>
+                                    </Container>
+                                </Row>
+                            </Form.Group>
+                            <Row><div></div></Row>
+
+                        </Container>
                     </Form>
                 </div>
 
-
-
-                <Container fluid style={{ width: '80vw', padding: '0' }}>
-                    <Row style={{ marginLeft: '0', marginRight: '0', width: '100%' }}>
-                        <InputForm
-                            id={1} //*ID позиции в анкете
-                            width={4} //* ширина поля
-                            show={true} //* видимость поля (true / false)
-                            verify="number" //* верификация числовых полей ('number' / "")
-                            label="Полное наименование предприятия"
-                            placeholder="information" //* информация о поле, для базы
-                            description="Position" //* описание поля
-                            value={value} //* значение передаваемое с сервера
-                            onChangeValue={this.doChangeValue} //* функция передающая STORE 
-                        />
-                        <InputForm
-                            id={1}
-                            width={4}
-                            show={true}
-                            verify="number"
-                            label="Полное наименование предприятия"
-                            placeholder="information"
-                            description="Position"
-                            value={value}
-                            onChangeValue={this.doChangeValue}
-                        />
-
-                        <SelectForm
-                            id={2}
-                            width={4}
-                            show={true}
-                            label="Полное наименование предприятия"
-                            placeholder="information"
-                            description="Position"
-                            option="position1, position2, position3, position4" //*список для выбора
-                            value="position1"
-                            onChangeValue={this.doChangeValue}
-                        />
-                    </Row>
-                    <Row>
-                        <CheckForm
-                            id={3}
-                            width={4}
-                            show={true}
-                            placeholder="information" //* подпись LABEL для чекбокса
-                            description="Position"
-                            value={false}
-                            onChangeValue={this.doChangeValue}
-                        />
-                    </Row>
-                </Container>
             </>
         )
     }
